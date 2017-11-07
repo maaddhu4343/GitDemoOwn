9e6afb3de1c43a7c1397ba7773286c5e1153771e
Madhu Babu <Madhu Babu@192.168.2.15>
2017-11-07 20:28:54 +0530

    first commit
    Change-Id: I5db6cb5ecb7c08b362a91c151834b14c5e0af7ee
    Signed-off-by: Madhu Babu <Madhu Babu@192.168.2.15>

---
+++ gittest/src/test/java/guru99/gittest/DEmomomoTest.java
@@ -0,0 +1,13 @@
+package guru99.gittest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class DEmomomoTest {
+	@Test
+	private void pu() {
+		WebDriver driver = new FirefoxDriver();
+	}
+
+}
---
+++ gittest/src/test/java/guru99/gittest1/AppTest.java
@@ -0,0 +1,14 @@
+package guru99.gittest1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+@Test
+private void pub() {
+	WebDriver driver = new FirefoxDriver();
+	
+
+}
+}
